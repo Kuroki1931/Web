7586a39cd8bd2dca3074f6fbbf946c8d88fc4dc5
kuroki <skuroki@fracta.ai>
2021-02-07 12:53:42 +0900

    modified

--- react-django/react_django/ap1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Ap1Config(AppConfig):
-    name = 'ap1'
--- react-django/todo/django/todoapp/views.py
+++
@@ -1,9 +0,0 @@
-from .models import Todo
-from .serializers import TodoSerializer
-from rest_framework import generics
-
-
-class ListTodo(generics.ListCreateAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
-
